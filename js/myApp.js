935fa84cc2b321c41976508a8bd9294f6c0e01ad
a-minor-threat <dennis.k.shah@gmail.com>
2015-12-25 11:37:54 -0500

    added controller

---
+++ js/myApp.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+  .controller('ML-Ctrl', function($scope){
+    $scope.MaleName='Male Name';
+    $scope.DirtyTask='Dirty Task';
+    $scope.OC='Obnoxious Celebrity';
+    $scope.JobTitle='Job Title';
+    $scope.Celebrity='Celebrity';
+    $scope.HN='Huge Number';
+    $scope.TediousTask='Tedious Task';
+    $scope.UselessSkill='Useless Skill';
+    $scope.Adjective='Adjective';
+  });
