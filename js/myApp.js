f42c531c714be8d5d00bca31f5e8564e328fa1ba
a-minor-threat <dennis.k.shah@gmail.com>
2015-12-28 18:52:40 -0500

    now accounts for gender (binary)

--- js/myApp.js
+++ js/myApp.js
@@ -1,6 +1,7 @@
 angular.module('myApp', [])
   .controller('ML-Ctrl', function($scope){
-    $scope.MaleName='Male Name';
+    $scope.maleName='Male Name';
+    $scope.femaleName='Female Name';
     $scope.DirtyTask='Dirty Task';
     $scope.OC='Obnoxious Celebrity';
     $scope.JobTitle='Job Title';
@@ -9,4 +10,10 @@ angular.module('myApp', [])
     $scope.TediousTask='Tedious Task';
     $scope.UselessSkill='Useless Skill';
     $scope.Adjective='Adjective';
+    $scope.Gender='M';
+    $scope.$watch('Gender', 
+    function(newValue, oldValue){
+      console.log(newValue);
+      
+    });
   });
